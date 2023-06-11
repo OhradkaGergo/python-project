7594407000ed2f46b207ad4900a26cd3ffd0aa90
OhradkaGergo <ohradkagergo@turr.hu>
2023-06-11 12:07:56 +0200

    fejlesztés

--- kód/szines_jatek.py
+++ kód/szines_jatek.py
@@ -1,6 +1,63 @@
 from tkinter import *
 import random
 
+szines_j_ablak = Tk()
+szines_j_ablak.title("Színes játék")
+szines_j_ablak.minsize(width = 600, height = 400)
+szines_j_ablak.maxsize(width = 600, height = 400)
+
+menusor = Frame(szines_j_ablak, width=600, height=400, bd=10, highlightbackground="green", highlightthickness=10)
+menusor.pack(side = TOP, fill = X)
+
+menu1 = Menubutton(menusor, text = "Főoldal", underline = 0, bg="#EC4949", width=15, height=2)
+menu1.pack(side = LEFT)
+dashboard = Menu(menu1,tearoff="off")
+menu1.config(menu = dashboard)
+
+menu3 = Menubutton(menusor, text = "Benzin számító", underline = 0,bg="#EC4949", width=15, height=2)
+menu3.pack(side = LEFT)
+likes = Menu(menu3, tearoff="off")
+likes  .add_command(label = "Benzin", underline = 0, background="#EC4949")
+likes.add_command(label = "Diesel", underline = 0,background="#EC4949")
+menu3.config(menu = likes)
+
+menu2 = Menubutton(menusor, text = "Adószámító", underline = 0,bg="#EC4949", width=15, height=2)
+menu2.pack(side = LEFT)
+views = Menu(menu2, tearoff="off")
+views.add_command(label = "Adószámító", underline = 0, background="#EC4949")
+views.add_command(label = "Kenyérszámító", underline = 0,background="#EC4949")
+menu2.config(menu =views)
+
+menu4 = Menubutton(menusor, text = "Spar", underline = 0,bg="#EC4949", width=15, height=2)
+menu4.pack(side = LEFT)
+uploads = Menu(menu4 ,tearoff="off")
+
+uploads.add_command(label = "Hőmérséklet váltó", underline = 0, background="#EC4949")
+uploads.add_command(label = "Pénznem váltó", underline = 0,background="#EC4949")
+uploads.add_command(label = "Mértékegység váltó", underline = 0,background="#EC4949")
+uploads.add_command(label = "Terület/Kerület", underline = 0, background="#EC4949")
+uploads.add_command(label = "URL rövidítő", underline = 0,background="#EC4949")
+uploads.add_command(label = "Jelszó erősség",  underline = 0,background="#EC4949")
+
+menu4.config(menu = uploads)
+
+menu5 = Menubutton(menusor, text = "Játékok", underline = 0,bg="#EC4949", width=15, height=2)
+menu5.pack(side = LEFT)
+videos = Menu(menu5,tearoff="off")
+videos.add_command(label = "Színes játék", underline = 0, background="#EC4949")
+videos.add_command(label = "Vízesés modell quiz", underline = 0,background="#EC4949")
+
+menu5.config(menu = videos)
+
+menu6 = Menubutton(menusor, text = "Kilépés", underline = 0,bg="#EC4949", width=15, height=2)
+menu6.pack(side = LEFT)
+videos = Menu(menu6,tearoff="off", )
+videos.add_command(label = "Kilépés", command = szines_j_ablak.destroy, underline = 0,background="#EC4949")
+menu6.config(menu = videos)
+
+ize2=Label(text="Sponsored by SPAR ™", fg="green",font=('Silkscreen', 8))
+ize2.place(x=30,y=350)
+
 szinek = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "White"]
 ido_hatra = 45
 pontszam = 0
@@ -61,11 +118,6 @@ def szamlalas():
                 objektiv = Label(szines_j_win, text = "VEDD MEG A SPART!!!!!", font = ('Helvetica', 15))
                 objektiv.pack()
 
-szines_j_ablak = Tk()
-szines_j_ablak.title("Színes játék")
-szines_j_ablak.minsize(width = 600, height = 400)
-szines_j_ablak.maxsize(width = 600, height = 400)
-
 objektiv = Label(szines_j_ablak, text = "Írd be, hogy milyen színűek a betűk, és nem azt, amit kiír!", font = ('Helvetica', 15))
 objektiv.pack()
 pontszam_label = Label(szines_j_ablak, text = "Nyomj egy \"enter\"-t a játék kezdéséért!", font = ('Helvetica', 12))
@@ -78,6 +130,5 @@ enter_beker = Entry(szines_j_ablak)
 szines_j_ablak.bind('<Return>', jatekKezdes)
 enter_beker.pack()
 enter_beker.focus_set()
-
 
 szines_j_ablak.mainloop()
